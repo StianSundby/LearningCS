06d86bb4b3f9ba1ae54c4518b010681389f22382
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-18 17:57:19 +0100

    Added WeekThree.cs

---
+++ LearningCS/WeekThree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCS
+{
+    internal class WeekThree
+    {
+        private static readonly Random Random = new();
+
+        private static void ReturnToPreviousMenu()
+        {
+            Console.WriteLine("\nThat's it. Press any key to return...");
+            Console.ReadKey(true);
+            GoBack();
+        }
+
+        public static void GoBack()
+        {
+            Console.Clear();
+            Program.ChooseTask("WeekThree");
+        }
+
+        public static void Task1()
+        {
+            Console.Clear();
+            Console.WriteLine("Task 1:\n" +
+                              "We were tasked with making a program that reads and handles data from a text file.\n"+
+                              "And with this data we were to make a word-riddle generator. All of the riddles are in"+
+                              "Norwegian.");
+
+        }
+    }
+}
