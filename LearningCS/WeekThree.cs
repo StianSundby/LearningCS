d125d3f966dd0127104a0fad2d196b6e7467b067
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-18 18:53:15 +0100

    Finished Week 3 - Task 1

--- LearningCS/WeekThree.cs
+++ LearningCS/WeekThree.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace LearningCS
 {
@@ -30,7 +29,63 @@ namespace LearningCS
                               "We were tasked with making a program that reads and handles data from a text file.\n"+
                               "And with this data we were to make a word-riddle generator. All of the riddles are in"+
                               "Norwegian.");
+            var words = ImportWords();
+            var count = 200;
+            while (count > 0)
+            {
+                var findPair = FindPair(words);
+                if (findPair) count--;
+            }
+            ReturnToPreviousMenu();
 
+            static string[] ImportWords()
+            {
+                var previousWord = string.Empty;
+                const string filePath = @"D:\Repos\LearningCS\LearningCS\Resources\wordlist.txt";
+                var words = new List<string>();
+                foreach (var line in File.ReadLines(filePath, Encoding.UTF8))
+                {
+                    var segment = line.Split('\t');
+                    var word = segment[1];
+                    if (word != previousWord && word.Length is > 6 and < 10 && !word.Contains("-"))
+                    {
+                        words.Add(word);
+                    }
+
+                    previousWord = word;
+                }
+
+                return words.ToArray();
+            }
+
+            static bool FindPair(IReadOnlyList<string> words)
+            {
+                var randomIndex = Random.Next(words.Count);
+                var currentWord = words[randomIndex];
+                Console.Write(currentWord + " - ");
+                foreach (var word in words)
+                {
+                    if (!LastEqualFirst(currentWord, word)) continue;
+                    Console.Write(word);
+                    Console.WriteLine();
+                    return true;
+                }
+
+                Console.WriteLine("Couldn't find a matching pair");
+                return false;
+            }
+
+            static bool LastEqualFirst(string currentWord, string word)
+            {
+                return CheckForMatch(4, currentWord, word) || CheckForMatch(5, currentWord, word);
+            }
+
+            static bool CheckForMatch(int length, string currentWord, string word)
+            {
+                var currentWordLastPart = currentWord.Substring(currentWord.Length - length, length);
+                var wordFirstPart = word[..length];
+                return currentWordLastPart == wordFirstPart;
+            }
         }
     }
 }
