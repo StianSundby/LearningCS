d758554f28f1542432058af55f78eea2516f04fe
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-20 21:49:14 +0100

    Added handling for Week 3 - Task 3

---
+++ LearningCS/TaskClasses/TextPrinter.cs
@@ -0,0 +1,31 @@
+﻿//Week 3 - Task 2
+
+using System;
+
+namespace LearningCS.TaskClasses
+{
+    public class TextPrinter
+    {
+        public static string Name { get; set; }
+        public static int Amount { get; set; }
+
+        public TextPrinter(string name, int amount)
+        {
+            Name = name;
+            Amount = amount;
+        }
+        public void PrintName()
+        {
+            Console.WriteLine(Name);
+            PrintText(Amount);
+        }
+
+        private static void PrintText(int amount)
+        {
+            for (var i = 1; i < amount + 1; i++)
+            {
+                Console.WriteLine("Round number: " + i);
+            }
+        }
+    }
+}
---
+++ LearningCS/TaskClasses/WelcomeMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//Week 3 - Task 3
+namespace LearningCS.TaskClasses
+{
+    internal class WelcomeMessage
+    {
+        public static string Name { get; set; }
+        public static string Message { get; set; }
+
+        public WelcomeMessage(string message)
+        {
+            Message = message;
+        }
+
+        public void PrintWelcomeMessage(string name)
+        {
+            Name = ", " + name;
+            Console.WriteLine(Message + Name);
+        }
+    }
+}
