2283ec9151dd44a5f29496f38ded0acad2436a83
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-24 14:41:47 +0100

    Menu handling. Finished Task1

--- LearningCS/WeekFour.cs
+++ LearningCS/WeekFour.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Channels;
+using LearningCS.Resources.TaskClasses.Matches;
 
 namespace LearningCS
 {
@@ -14,12 +16,31 @@ namespace LearningCS
         public static void GoBack()
         {
             Console.Clear();
-            Program.ChooseTask("WeekThree");
+            Program.ChooseTask("WeekFour");
         }
 
         public static void Task1()
         {
-            
+            Console.Clear();
+            Console.WriteLine(Properties.Resources.WeekFour_Task1_Intro);
+            Console.ReadKey(true);
+            Console.WriteLine("Slight warning: this code is abit janky and may cause some exception errors... Best of luck :)");
+            Console.Write("Valid bets: \r\n - H, D, A\r\n - split: HD, HA, DA\r\n - full: HDA\r\nEnter your 12 bets with a comma as a seperator:");
+            var betsText = Console.ReadLine();
+            var rounds = new Rounds(betsText);
+            while (true)
+            {
+                Console.Write("Enter round number 1-12 for the score or X to exit\r\nEnter command:");
+                var command = Console.ReadLine();
+                if (command == "X") break;
+                var matchNo = Convert.ToInt32(command);
+                Console.Write($"Scoring i kamp {matchNo}. \r\nEnter H for hometeam or A for awayteam:");
+                var team = Console.ReadLine();
+                rounds.AddGoal(matchNo, team == "H");
+                rounds.Score();
+                rounds.CheckWins();
+            }
+            ReturnToPreviousMenu();
         }
     }
 }
