e1471908dc2fcde592d4bd79796d7cdac757173d
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-24 14:41:20 +0100

    Week4 - Task1 specific class

---
+++ LearningCS/Resources/TaskClasses/Matches/Match.cs
@@ -0,0 +1,32 @@
+﻿namespace LearningCS.Resources.TaskClasses.Matches
+{
+    internal class Match
+    {
+        private int _homeGoals;
+        private int _awayGoals;
+        private readonly string _bet;
+        
+
+        public Match(string bet)
+        {
+            _bet = bet;
+        }
+
+        public void Goal(bool homeTeam)
+        {
+            if (homeTeam) _homeGoals++;
+            else _awayGoals++;
+        }
+
+        public bool CheckBet()
+        {
+            var result = _homeGoals == _awayGoals ? "U" : _homeGoals > _awayGoals ? "H" : "A";
+            return _bet.Contains(result);
+        }
+
+        public string GetScore()
+        {
+            return _homeGoals + "-" + _awayGoals;
+        }
+    }
+}
---
+++ LearningCS/Resources/TaskClasses/Matches/Rounds.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace LearningCS.Resources.TaskClasses.Matches
+{
+    internal class Rounds
+    {
+        public bool GameRunning { get; private set; }
+        private readonly Match[] _rounds;
+
+        public Rounds(string userBet)
+        {
+            GameRunning = true;
+            var bets = userBet.Split(',');
+            _rounds = new Match[12];
+            for (var i = 0; i < 12; i++)
+            {
+                _rounds[i] = new Match(bets[i]);
+            }
+        }
+
+        public bool Stop()
+        {
+            
+            return GameRunning;
+        }
+
+        public void AddGoal(int roundNumber, bool homeTeam)
+        {
+            var index = roundNumber - 1;
+            var currentRound = _rounds[index];
+            currentRound.Goal(homeTeam);
+        }
+
+        public void Score()
+        {
+            for (var index = 0; index < _rounds.Length; index++)
+            {
+                var round = _rounds[index];
+                var matchNo = index + 1;
+                var checkBet = round.CheckBet();
+                var checkBetOutput = checkBet ? "correct" : "wrong";
+                Console.WriteLine($"Round {matchNo}: {round.GetScore()} - {checkBetOutput}");
+            }
+        }
+
+        public void CheckWins()
+        {
+            var currentRound = _rounds.Count(round => round.CheckBet());
+            Console.WriteLine($"You have {currentRound} correct bets.");
+        }
+    }
+}
