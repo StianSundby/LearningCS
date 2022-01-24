42e446e41474d09822224f4e113b2d99cfd9a125
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-24 11:15:49 +0100

    Added WeekFour.cs with the standard methods

---
+++ LearningCS/WeekFour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LearningCS
+{
+    internal class WeekFour
+    {
+        private static void ReturnToPreviousMenu()
+        {
+            Console.WriteLine(Properties.Resources.ReturnToPreviousMenu);
+            Console.ReadKey(true);
+            GoBack();
+        }
+
+        public static void GoBack()
+        {
+            Console.Clear();
+            Program.ChooseTask("WeekThree");
+        }
+
+        public static void Task1()
+        {
+            
+        }
+    }
+}
