e61d842e4e3310b2fa0d0991b5205cbee6302454
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-26 10:06:38 +0100

    Added task specific class for Week 4 - Task 2

---
+++ LearningCS/Resources/TaskClasses/BossFight.cs
@@ -0,0 +1,18 @@
+﻿namespace LearningCS.Resources.TaskClasses
+{
+    internal class BossFight
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Stamina { get; set; }
+
+        public BossFight(string name, int health, int strength, int stamina)
+        {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+        }
+    }
+}
