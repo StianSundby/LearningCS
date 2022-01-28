3d48c0ab3647416f33e37f3bbcf0295762b7b3ba
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-28 16:43:01 +0100

    Finished Task 3

--- LearningCS/Weeks/WeekFour.cs
+++ LearningCS/Weeks/WeekFour.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using LearningCS.Resources.TaskClasses;
 using LearningCS.Resources.TaskClasses.Matches;
 
-namespace LearningCS
+namespace LearningCS.Weeks
 {
     internal class WeekFour
     {
@@ -110,6 +110,174 @@ namespace LearningCS
             {
                 var randomStrength = new Random().Next(min, max);
                 return randomStrength;
+            }
+        }
+
+        public static void Task3()
+        {
+            Console.Clear();
+            Console.WriteLine(Properties.Resources.WeekFour_Task3_Intro);
+            Console.ReadKey(true);
+
+            #region Initialize
+            int mode;
+            char xo;
+            char computerChar;
+            InitializeGame();
+
+            char[] a = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            var turn = 1;
+            var gameStatus = 0;
+            #endregion
+            do
+            {
+                #region AlwaysOnScreen
+                Console.Clear();
+                switch (mode)
+                {
+                    case 1:
+                        Console.WriteLine("Player:X and Computer:O");
+                        Console.WriteLine("\n");
+                        Console.WriteLine(turn % 2 == 0 ? "Computers turn" : "Players turn");
+                        break;
+                    case 2:
+                        Console.WriteLine("Player1:X and Player2:O");
+                        Console.WriteLine("\n");
+                        Console.WriteLine(turn % 2 == 0 ? "Player 2' turn" : "Player 1's turn");
+                        break;
+                }
+                Board();
+                #endregion
+
+                int selectedSquare;
+                switch (mode)
+                {
+                    case 1:
+                        #region PlayersTurn
+
+                        if (turn % 2 == 0) goto case 2;
+                        Console.WriteLine("Where do you want to place your piece?");
+                        selectedSquare = int.Parse(Console.ReadLine() ?? string.Empty);
+
+                        if (a[selectedSquare] != 'X' && a[selectedSquare] != 'O')
+                        {
+                            a[selectedSquare] = char.ToUpper(xo);
+                            turn++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Square number {0} is already marked with an {1}", selectedSquare, a[selectedSquare]);
+                            Console.WriteLine("Please wait while the board reloads...");
+                            Thread.Sleep(1000);
+                        }
+                        #endregion
+                        gameStatus = CheckWin();
+                        break;
+
+                    case 2:
+                        #region ComputersTurn
+                        selectedSquare = new Random().Next(1, 9);
+                        while (a[selectedSquare] == 'X' || a[selectedSquare] == 'O')
+                        {
+                            selectedSquare = new Random().Next(1, 9);
+                        }
+
+                        a[selectedSquare] = char.ToUpper(computerChar);
+                        turn++;
+                        #endregion
+                        gameStatus = CheckWin();
+                        break;
+                }
+            } while (gameStatus != 1 && gameStatus != -1);
+
+            Console.Clear();
+            Board();
+
+            #region EndOfGameMessage
+            switch (gameStatus)
+            {
+                case 1:
+                    switch ((turn % 2) + 1)
+                    {
+                        case 1:
+                            Console.WriteLine("You won!");
+                            break;
+                        case 2:
+                            Console.WriteLine("The computer won!");
+                            break;
+                    }
+
+                    break;
+                case -1:
+                    Console.WriteLine("Draw!");
+                    break;
+            }
+            #endregion
+
+            ReturnToPreviousMenu();
+
+            void Board()
+            {
+                Console.WriteLine("     |     |      ");
+                Console.WriteLine("  {0}  |  {1}  |  {2}", a[1], a[2], a[3]);
+                Console.WriteLine("_____|_____|_____ ");
+                Console.WriteLine("     |     |      ");
+                Console.WriteLine("  {0}  |  {1}  |  {2}", a[4], a[5], a[6]);
+                Console.WriteLine("_____|_____|_____ ");
+                Console.WriteLine("     |     |      ");
+                Console.WriteLine("  {0}  |  {1}  |  {2}", a[7], a[8], a[9]);
+                Console.WriteLine("     |     |      ");
+            }
+
+            int CheckWin()
+            {
+                #region Horizontal
+                if (a[1] == a[2] && a[2] == a[3]) return 1;
+                if (a[4] == a[5] && a[5] == a[6]) return 1;
+                if (a[6] == a[7] && a[7] == a[8]) return 1;
+                #endregion
+                #region Vertical
+                if (a[1] == a[4] && a[4] == a[7]) return 1;
+                if (a[2] == a[5] && a[5] == a[8]) return 1;
+                if (a[3] == a[6] && a[6] == a[9]) return 1;
+                #endregion
+                #region Diagonal
+                if (a[1] == a[5] && a[5] == a[9]) return 1;
+                if (a[3] == a[5] && a[5] == a[7]) return 1;
+                #endregion
+                #region Draw
+                if (a[1] != '1' && a[2] != '2' && a[3] != '3'
+                    && a[4] != '4' && a[5] != '5' && a[6] != '6'
+                    && a[7] != '7' && a[8] != '8' && a[9] != '9') return -1;
+
+
+
+                #endregion
+                return 0;
+            }
+
+            void InitializeGame()
+            {
+                computerChar = '\0';
+
+                #region WriteLine
+                Console.WriteLine(
+                    "Choose a mode:\n" +
+                    "    1 - Player vs. Computer\n" +
+                    "    2 - Player1 vs. Player2\n\n" +
+                    "Enter your choice:"
+                );
+                #endregion
+                mode = int.Parse(Console.ReadLine() ?? string.Empty);
+
+                Console.WriteLine("Do you want to be X or O?");
+                xo = Convert.ToChar(Console.ReadLine()?.ToUpper()!);
+                computerChar = xo switch
+                {
+                    'X' => 'O',
+                    'O' => 'X',
+                    _ => computerChar
+                };
             }
         }
     }
