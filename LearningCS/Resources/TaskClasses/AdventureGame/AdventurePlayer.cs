343a4ebb8d1dfda7adcfa7b7e5d24b1dc92f7f21
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-31 09:54:28 +0100

    Task5 - player class

---
+++ LearningCS/Resources/TaskClasses/AdventureGame/AdventurePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace LearningCS.Resources.TaskClasses.AdventureGame
+{
+    internal class AdventurePlayer
+    {
+        public List<string> Have;
+        public Room Room;
+
+        public AdventurePlayer()
+        {
+            Have = new List<string>();
+        }
+
+        public string Inventory()
+        {
+            return string.Join("\n", Have);
+        }
+    }
+}
