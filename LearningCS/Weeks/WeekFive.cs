b3d66559697afcd709e51ff4fa834d32366cb29e
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-31 09:52:19 +0100

    Added WeekFive.cs

---
+++ LearningCS/Weeks/WeekFive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LearningCS.Weeks
+{
+    internal class WeekFive
+    {
+        private static void ReturnToPreviousMenu()
+        {
+            Console.WriteLine(Properties.Resources.ReturnToPreviousMenu);
+            Console.ReadKey(true);
+            GoBack();
+        }
+
+        public static void GoBack()
+        {
+            Console.Clear();
+            Program.ChooseTask("WeekFour");
+        }
+
+        public static void Task1()
+        {
+            Console.Clear();
+        }
+    }
+}
