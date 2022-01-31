6420c3081c2a5afc1ddde19e504b3a1464d51b61
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-31 09:53:21 +0100

    Task5 - Class for rooms

---
+++ LearningCS/Resources/TaskClasses/AdventureGame/Room.cs
@@ -0,0 +1,15 @@
+﻿namespace LearningCS.Resources.TaskClasses.AdventureGame
+{
+    internal class Room
+    {
+        public string Name;
+        public string[] Content;
+        public bool Start;
+        public bool Won;
+
+        public string GetContent()
+        {
+            return string.Join("\n", Content);
+        }
+    }
+}
