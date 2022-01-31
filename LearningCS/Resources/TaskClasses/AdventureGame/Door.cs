929d434b73c2e38d45486b1231b1f7ed32ebaf87
Stian <88629622+StianSundby@users.noreply.github.com>
2022-01-31 09:54:17 +0100

    Task5 - door class

---
+++ LearningCS/Resources/TaskClasses/AdventureGame/Door.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningCS.Resources.TaskClasses.AdventureGame
+{
+    internal class Door
+    {
+        public Room A;
+        public Room B;
+        public string color;
+        public bool open;
+    }
+}
