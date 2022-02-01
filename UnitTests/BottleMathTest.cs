6e5952ee610991fae8e071a98fff99e8111c1b9d
Stian <88629622+StianSundby@users.noreply.github.com>
2022-02-01 19:05:23 +0100

    Unit test for WeekFive - Task 3

---
+++ UnitTests/BottleMathTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using LearningCS.Resources.TaskClasses.BottleMath;
+
+namespace UnitTests
+{
+    internal class BottleMathTest
+    {
+        [Test]
+        public void BottleMathTest1()
+        {
+            var simulation = new Simulation( 1, 5, 7);
+            var operationSet = simulation.Run();
+            Assert.AreEqual(8, operationSet.Length);
+            Assert.AreEqual(1, simulation.Bottle1.Content);
+            Assert.AreEqual(7, simulation.Bottle2.Content);
+        }
+        [Test]
+        public void BottleMathTest2()
+        {
+            var simulation = new Simulation(1, 3, 5);
+            var operationSet = simulation.Run();
+            Assert.AreEqual(4, operationSet.Length);
+            Assert.AreEqual(1, simulation.Bottle1.Content);
+            Assert.AreEqual(5, simulation.Bottle2.Content);
+        }
+        [Test]
+        public void BottleMathTest3()
+        {
+            var simulation = new Simulation(2, 4, 5);
+            var operationSet = simulation.Run();
+            Assert.AreEqual(6, operationSet.Length);
+            Assert.AreEqual(4, simulation.Bottle1.Content);
+            Assert.AreEqual(2, simulation.Bottle2.Content);
+        }
+    }
+}
