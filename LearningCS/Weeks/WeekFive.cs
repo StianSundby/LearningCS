0f1a565f52366ebc9ccbe511b09b924c484c9d4d
Stian <88629622+StianSundby@users.noreply.github.com>
2022-02-01 14:18:12 +0100

    Imported Linq and task specific class. Added Task 2

--- LearningCS/Weeks/WeekFive.cs
+++ LearningCS/Weeks/WeekFive.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using LearningCS.Resources.TaskClasses.BottleMath;
 
 namespace LearningCS.Weeks
 {
@@ -21,6 +23,36 @@ namespace LearningCS.Weeks
         {
             Console.Clear();
             Console.WriteLine(Properties.Resources.WeekFive_Task1_Intro);
+            Console.ReadKey(true);
+            ReturnToPreviousMenu();
+        }
+
+        public static void Task2()
+        {
+            Console.Clear();
+            Console.WriteLine(Properties.Resources.WeekFive_Task2_Intro);
+            Console.ReadKey(true);
+            Console.WriteLine(Properties.Resources.WeekFive_Task2_Description);
+
+            var input = Console.ReadLine();
+            if (input != null && input.Contains(' ')) input.Split(' ');
+            else if (input != null && !input.Contains(' ')) input.ToArray();
+            else
+            {
+                Console.WriteLine("Input was not in correct format.\nPress any key to cancel...");
+                Console.ReadKey(true);
+                ReturnToPreviousMenu();
+            }
+            Console.Clear();
+
+            var wantedResult = Convert.ToInt32(input[0]);
+            var bottle1 = Convert.ToInt32(input[1]);
+            var bottle2 = Convert.ToInt32(input[2]);
+            var simulation = new Simulation(wantedResult, bottle1, bottle2);
+            var operationSet = simulation.Run();
+
+            Console.WriteLine(operationSet.GetDescription());
+            Console.WriteLine("\n\nPress any key to return...");
             Console.ReadKey(true);
             ReturnToPreviousMenu();
         }
